fcd19916867e420e07e4be0a3c90df15f0b91281
Kyle Durand <kyledurand@gmail.com>
2024-06-19 16:38:38 -0400

    feat: add images to travels, update layout

--- src/data/travels.ts
+++ src/data/travels.ts
@@ -1,68 +1,93 @@
+import india from "../images/travels/india.jpeg";
+import japan from "../images/travels/japan.jpeg";
+import southernAfrica from "../images/travels/southern-africa.jpeg";
+import morocco from "../images/travels/morocco.jpeg";
+import china from "../images/travels/china.jpeg";
+import hawaii from "../images/travels/hawaii.jpeg";
+import utah from "../images/travels/utah.jpeg";
+import azores from "../images/travels/azores.jpeg";
+import whistler from "../images/travels/whistler.jpeg";
+import iceland from "../images/travels/iceland.jpeg";
+import scotland from "../images/travels/scotland.jpeg";
+import northernItaly from "../images/travels/northern-italy.jpeg";
+
 export const travels = [
   {
     year: 2013,
+    image: india,
     location: "India",
     description:
       "New Delhi, Agra, Jaipur, Udaipur, Mumbai, Goa, Kochi, and Varkala.",
   },
   {
     year: 2014,
+    image: japan,
     location: "Japan",
     description:
       "Tokyo, Takayama, Kanazawa, Kyoto, Hiroshima, Miyajima, Takamatsu, and Osaka.",
   },
   {
     year: 2015,
+    image: southernAfrica,
     location: "Southern Africa",
     description:
       "Cape Town, Orange River, Fish River Canyon, Namib Desert, Swakopmund, Skeleton coast, Spitzkoppe, Etosha National Park, Okavango Delta, Zambia, and Livingstone / Victoria Falls.",
   },
   {
     year: 2016,
+    image: morocco,
     location: "Morocco / Spain",
     description:
       "Marrakech, Essaouira, Seville, Tarifa, Malaga, Granada, Valencia, and Barcelona.",
   },
   {
     year: 2017,
+    image: china,
     location: "China",
     description:
       "Beijing, Xi'an, Suzhou , Shanghai, Wuyishan, Xiamen, Shenzen, and Hong Kong.",
   },
   {
     year: 2018,
+    image: hawaii,
     location: "Hawaii",
     description: "Oahu, Kona, Hilo, Hawi, Mona Kea, and lower Puna eruption.",
   },
   {
     year: 2019,
+    image: utah,
     location: "U.S. Road trip",
     description:
       "ON > NY > PA > OH > KY > TN > AR > OK > TX > NM > AZ > CA > NV > UT > WY > NE > IA > IL > ON.",
   },
   {
     year: 2019,
+    image: azores,
     location: "Azores / Portugal",
     description: "Sao Miguel, Terceira, Lisbon, and Porto.",
   },
   {
     year: 2020,
+    image: whistler,
     location: "Vancouver / Whistler / Maui / Mont Tremblant / Cottage",
     description:
       "Married in Whistler with mini-moon in Maui. Arrived home 3 days before lockdowns started.",
   },
   {
     year: 2021,
+    image: iceland,
     location: "Spain / Iceland",
     description: "Mallorca, Barcelona, and Reykjavik",
   },
   {
     year: 2022,
+    image: scotland,
     location: "England / Scotland",
     description: "London, Edinburgh, and Isle of Skye",
   },
   {
     year: 2023,
+    image: northernItaly,
     location: "Northern Italy",
     description: "Milan, Bergamo, Cortina d'Ampezzo, Verona, and Lake Como",
   },
