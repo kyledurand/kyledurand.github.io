e8194ef25918b4507f473cd16e5fe1f52d5905e0
Kyle Durand <kyle.durand@shopify.com>
2024-06-23 16:36:20 -0400

    chore: add astro-compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import yaml from '@rollup/plugin-yaml';
+import {defineConfig} from "astro/config";
+import yaml from "@rollup/plugin-yaml";
 import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {plugins: [yaml()]},
-  integrations: [lit()],
-  site: 'https://kyledurand.github.io',
+  integrations: [lit(), (await import("astro-compress")).default()],
+  site: "https://kyledurand.github.io",
 });
