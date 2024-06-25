5ffa257ac5a6d56dc2b3c0b3ff0b72b95bbde01e
Kyle Durand <kyle.durand@shopify.com>
2024-06-25 17:48:40 -0400

    chore: remove yaml support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import {defineConfig} from "astro/config";
-import yaml from "@rollup/plugin-yaml";
 import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {plugins: [yaml()]},
   integrations: [lit()],
   site: "https://kyledurand.github.io",
 });
