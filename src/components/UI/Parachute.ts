5585391996073ff3e26c27f414c533b9829dda0f
Kyle Durand <kyle.durand@shopify.com>
2024-06-25 17:45:46 -0400

    chore: update parachute load directive

--- src/components/UI/Parachute.ts
+++ src/components/UI/Parachute.ts
@@ -1,5 +1,5 @@
-import { LitElement, css, html } from "lit";
-import { customElement, state } from "lit/decorators.js";
+import {LitElement, css, html} from "lit";
+import {customElement, state} from "lit/decorators.js";
 
 @customElement("ui-parachute")
 export class UIParachute extends LitElement {
